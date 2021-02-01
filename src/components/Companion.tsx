a281d2986cd6888512fd222f54e54aa6c0847875
Jason <hareguu89@gmail.com>
2021-02-01 19:32:28 +0900

    fix-background

--- src/components/Companion.tsx
+++ src/components/Companion.tsx
@@ -306,8 +306,6 @@ const ContentContainer = styled.div`
   width: 100%;
   padding-left: 10rem;
   padding-right: 10rem;
-  background: url(https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80);
-
   .links {
     color: black;
     font-size: 12px;
